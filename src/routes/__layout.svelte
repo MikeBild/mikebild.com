d3026e78659c2c054a24330bf7bb09278bd98d3d
Mike Bild <mike.bild@googlemail.com>
2021-08-19 17:13:26 +0200

    fix layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -16,7 +16,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" as="style" />
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
-</svelte:head>svelte:head>
+</svelte:head>
 
 {#if !loaded}
   <section class="overflow-x-hidden w-screen relative">
